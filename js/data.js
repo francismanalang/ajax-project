080483e442f71df6f30afc0c5728016551a6bb2f
Francis Manalang <francismanalang7@gmail.com>
2022-08-02 00:12:35 +0000

    Generate a random country and flag. #1

--- js/data.js
+++ js/data.js
@@ -1,1 +1,20 @@
 /* exported data */
+
+var data = {
+  view: 'globe',
+  saved: [],
+  nextEntryId: 1
+};
+
+var previousDataJSON = localStorage.getItem('atlas-local-storage');
+
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+
+function atlasStorage(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('atlas-local-storage', dataJSON);
+}
+
+window.addEventListener('beforeunload', atlasStorage);
